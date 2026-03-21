a940ee69060b9956128d80608f7b77461033ce9e
Rushikannan <kannan.rushi05@gmail.com>
2026-03-21 10:33:51 +0530

    Updated Bi-LSTM

--- src/dl/bi-lstm_model.py
+++ src/dl/bi-lstm_model.py
@@ -43,7 +43,7 @@ def preprocess(text):
     text = text.lower()
     text = re.sub(r'http\S+|www\S+', '', text)
     text = re.sub(r'\d+', '', text)
-    text = re.sub(r'[^a-z\s!?]', '', text)
+    text = re.sub(r'[^a-z\s]', '', text)
     text = re.sub(r'\s+', ' ', text).strip()
     return text
 
